3c7963300b61ca2f4fad142e5838d4d101096e68
Pierca <mpiercamilli@outlook.com>
2019-03-27 23:07:15 -0300

    Partir la UI en componentes

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -1,25 +1,61 @@
 import {Casilla, EstadoCasilla} from "../models/casilla.js";
 
-const filaHTML = `<div id="fila{0}" class="fila"></div>`;
-const casillaHTML = `<div id="{0}" class="{1}"></div>`;
 const estiloCasillaPrendida = "casilla prendida";
 const estiloCasillaApagada = "casilla apagada";
 
 const casillas: Casilla[][] = [];
 const tamano: number = 3;
 
+function get(url: string): Promise<any> {
+    const request = new XMLHttpRequest();
+
+    return new Promise((resolve, reject) => {
+        request.open("GET", url);
+
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 300) {
+                resolve(request.response);
+            } else {
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
+            }
+        };
+
+        request.onerror = () => {
+            reject({
+                status: request.status,
+                statusText: request.statusText
+            });
+        };
+        
+        request.send();
+    });
+}
+
 window.onload = () => {
     crearTablero();
 }
 
-function crearTablero(): void {
+function crearFila(): Promise<string> {
+    return get("public/views/fila.html");
+}
+
+function crearCasilla(): Promise<string> {
+    return get("public/views/casilla.html");
+}
+
+async function crearTablero(): Promise<void> {
     const tablero = document.getElementById("tablero");
+    const filaHTML = await crearFila().then((fila: string) => fila);
+    const casillaHTML = await crearCasilla().then((casilla: string) => casilla);
 
     for(let i = 0; i< tamano; i++) {
         casillas.push([]);        
         const nuevaFila = filaHTML.replace("{0}", i.toString());
         tablero.innerHTML += nuevaFila;
-        const filaElement = document.getElementById(`fila${i}`);
+        const filaElement = document.getElementById(`fila${i}`) || new Element();
 
         for(let j = 0; j< tamano; j++) {
             const estadoInicial: EstadoCasilla = Math.round(Math.random());
@@ -29,10 +65,10 @@ function crearTablero(): void {
 
             switch (estadoInicial) {
                 case EstadoCasilla.Apagada:
-                    nuevaCasilla = nuevaCasilla.replace("{1}", estiloCasillaApagada);
+                    nuevaCasilla = nuevaCasilla && nuevaCasilla.replace("{1}", estiloCasillaApagada) || "";
                     break;
                 case EstadoCasilla.Prendida:
-                    nuevaCasilla = nuevaCasilla.replace("{1}", estiloCasillaPrendida);
+                    nuevaCasilla = nuevaCasilla && nuevaCasilla.replace("{1}", estiloCasillaPrendida) || "";
                     break;
                 default:
                     break;
@@ -50,12 +86,7 @@ function crearTablero(): void {
 }
 
 function agregarEventos(): void {
-    casillas.forEach((fila) => {
-        fila.forEach((casilla) => {
-            const elemento = document.getElementById(casilla.id);
-            elemento.addEventListener("click", (evento: Event) => cambiarEstado(evento));
-        })
-    })
+    [].concat(...casillas).forEach((casilla: Casilla) => document.getElementById(casilla.id).addEventListener("click", (evento: Event) => cambiarEstado(evento)));
 }
 
 function cambiarEstado(evento: Event): void {
@@ -63,22 +94,22 @@ function cambiarEstado(evento: Event): void {
     const casillasACambiar = buscarCasillasACambiar(casilla);
 
     casillasACambiar.forEach(casillaACambiar => setearEstado(casillaACambiar));
+}
 
-    function setearEstado(casillaACambiar: Casilla){
-        const elemento = document.getElementById(casillaACambiar.id);
+function setearEstado(casillaACambiar: Casilla){
+    const elemento = document.getElementById(casillaACambiar.id);
 
-        switch (casillaACambiar.estado) {
-            case EstadoCasilla.Apagada:
-                casillaACambiar.estado = EstadoCasilla.Prendida;
-                elemento.className = estiloCasillaPrendida;            
-                break;
-            case EstadoCasilla.Prendida:
-                casillaACambiar.estado = EstadoCasilla.Apagada;
-                elemento.className = estiloCasillaApagada;
-                break;
-            default:
-                break;
-        }
+    switch (casillaACambiar.estado) {
+        case EstadoCasilla.Apagada:
+            casillaACambiar.estado = EstadoCasilla.Prendida;
+            elemento.className = estiloCasillaPrendida;            
+            break;
+        case EstadoCasilla.Prendida:
+            casillaACambiar.estado = EstadoCasilla.Apagada;
+            elemento.className = estiloCasillaApagada;
+            break;
+        default:
+            break;
     }
 }
 
